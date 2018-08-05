15d96b4ddc62864c38d8b1e9b65d43385d2b0897
ib00sh <wowmaxwow98@gmail.com>
2018-08-05 20:45:21 +0300

    add 2 btn and new Label

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,29 +1,53 @@
 package sample;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.application.*;
 import javafx.scene.*;
-import javafx.scene.control.Label;
-import javafx.scene.layout.FlowPane;
-import javafx.stage.Stage;
+import javafx.stage.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.event.*;
+import javafx.geometry.*;
+
 
 
 
 public class Main extends Application {
-
-    @Override
-    public void start(Stage myStage) throws Exception{
-        myStage.setTitle ("Demonstrate a JavaFX label");
-        FlowPane rootNode = new FlowPane();
-        Scene myScence = new Scene(rootNode, 300, 200);
-        myStage.setScene(myScence);
-        Label myLabel = new Label("This is a JavaFX label");
-        rootNode.getChildren().add(myLabel);
-        myStage.show();
-    }
-
+    Label response;
 
     public static void main(String[] args) {
         launch(args);
     }
+
+    @Override
+    public void start(Stage myStage) throws Exception{
+        myStage.setTitle ("Demonstrate a JavaFX label");
+        FlowPane rootNode = new FlowPane(10,10);
+        rootNode.setAlignment(Pos.CENTER);
+        Scene myScence = new Scene(rootNode, 300, 100);
+        myStage.setScene(myScence);
+        response = new Label("Push a Button");
+
+        Button btnAlpha = new Button("Alpha");
+        Button btnBeta = new Button("Beta");
+
+        btnAlpha.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                response.setText("Alpha was pressed. ");
+            }
+        });
+
+        btnBeta.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                response.setText("Beta was pressed");
+            }
+        });
+
+        rootNode.getChildren().addAll(btnAlpha, btnBeta, response);
+        myStage.show();
+    }
+
+
+
 }
